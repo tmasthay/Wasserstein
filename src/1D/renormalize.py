56a573a8700bdd305b488d06cf7caf252a7f75f5
Tyler Masthay <tylermasthay@wireless-10-145-188-65.public.utexas.edu>
2020-03-09 16:26:18 -0500

    Replace Gauss quad with midpoint
    Fixed bug for CDF -- W2 seems to give desired convexity result, although maybe numerically a bit off

--- src/1D/renormalize.py
+++ src/1D/renormalize.py
@@ -7,6 +7,11 @@ from scipy import signal
 def qquad(f,a,b):
   return quad(f,a,b,limit=200)
 
+def trapz(ff, xx):
+  dx    = xx[1:] - xx[:-1]
+  ave_y = (ff[1:] + ff[:-1]) / 2
+  return np.dot(dx,ave_y)
+
 #splits wave f and g into positive and negative parts
 def split(f):
   def f_plus(x):
@@ -23,8 +28,16 @@ def split(f):
 
 #renormalize a given probability distribution
 def normalize(f, a, b):
-  print('integrate quad normalize')
-  C,D = quad(f,a,b)
+#  C,D = quad(f,a,b)
+  xx = np.linspace(a,b,1000)
+  yy = np.array(list(map(f,xx)))
+  C  = trapz(yy,xx)
+
+  print('Normalize: %s'%(C))
+  if( C == 0 ):
+    plt.plot(xx,yy)
+    plt.show()
+
   if( C == 0 ):
     return f
   return lambda x : f(x) / C
@@ -32,8 +45,6 @@ def normalize(f, a, b):
 #splits signal into 
 def split_normalize(f, a, b):
   f_p,f_m = split(f)  
-
-  print('split_normalize')
   f_p = normalize(f_p,a,b)
   f_m = normalize(f_m,a,b)
   return f_p, f_m
@@ -46,7 +57,6 @@ def better_split_normalize(f,g,a,b):
 
 def exp_normalize_full(f,g,gamma,a,b):
    h = lambda x : np.exp(-gamma * (f(x) - g(x)))
-   print('integrate quad')
    C = quad(h, a, b)
    return lambda x : h(x) / C
 
@@ -60,18 +70,11 @@ def normalize_L1(f,g,a,b):
   gg = lambda x : abs(g(x))
   return [(normalize(ff,a,b), normalize(gg,a,b))]
 
-#get cdf from pdf
-def cumulative(f):
-  print('integrate quad cumulative')
-  return lambda x : quad(f,-np.inf, x)
-#  return lambda x : quad(f, 0, x)
-
-
-def cum_disc(f,x):
+def cum_disc(f,x,left_endpoint=-np.inf):
   y    = np.zeros(len(x))
   errs = np.zeros(len(x))
 
-  tmp     = quad(f, -np.inf, x[0])
+  tmp     = quad(f, left_endpoint, x[0])
   y[0]    = tmp[0]
   errs[0] = tmp[1] 
   for i in range(1,len(x)):
@@ -79,62 +82,8 @@ def cum_disc(f,x):
     y[i]    = y[i-1] + tmp[0]
     errs[i] = tmp[1]
   return y,errs
-
-#cum split
-# Functionality: splits values from integral errors
-#  -- F -- cum. dist. in vectorized form
-""" 
-def cum_split(F):
-  vals = np.zeros(len(F))
-  errs = np.zeros(len(F))
-  for i in range(0,len(F)):
-    vals[i] = F[i][0]
-    errs[i] = F[i][1]
-  return vals,errs
-"""
-
-#DONT USE THIS!!! IT DOESNT WORK WELL!!
-def invert_cdf(F):
-  def the_inverse(y):
-    return fsolve(lambda x : F(x) - y,0)
-  return the_inverse
-
-def invert_cdf_disc_cts(F, xx, interpolate=False):
-  def G(y):
-    def opt_interval(ii):
-      return (F[ii] <= y and y <= F[ii+1])
-    step = len(xx) - 2
-    i    = 0 
-    while( True ):
-      if( step == 0 ):
-        if( i == 0 ):
-#          return (xx[i] + xx[i+1]) / 2
-           return xx[i]
-        if( i == len(xx) - 1 ):
-#          return (xx[i-1] + xx[i]) / 2
-           return xx[i]
-        return (xx[i-1] + xx[i] + xx[i+1]) / 3
-      if( opt_interval(i) ):
-        if( i == 0 or i == (len(xx) - 1) ):
-          return xx[i]
-        if( interpolate ):
-          return xx[i] + (y - F[i]) * (xx[i+1] - xx[i]) / (F[i+1] - F[i])
-        else:
-          return xx[i]
-      elif( F[i] > y ):
-        if( i == 0 ):
-          return xx[0]
-        i -= step
-      else:
-        if( i == len(xx) - 2 ):
-          return xx[len(xx)-1]
-        i += step
-      step //= 2 
-  return G
-      
+ 
 def invert_cdf_disc_disc(FF, xx, yy, interpolate=False):
-#  G = invert_cdf_disc_cts(F, xx, interpolate)
- # return np.array(list(map(G,yy)))
   return np.array(list(map(\
       lambda z : xx[min(len(FF)-1, np.digitize(z, FF, True))],yy))) 
 
@@ -145,12 +94,13 @@ def invert_cdf_disc_disc(FF, xx, yy, interpolate=False):
 def wasserstein_integrand(f,g,x,N,smooth=False):
    #Rewrite as F^{-1}(y) - G^{-1}(y)
    tol = 1.0e-05
-   F,F_err     = cum_disc(f,x)
-   G,G_err     = cum_disc(g,x)
+   F,F_err     = cum_disc(f,x,x[0])
+   G,G_err     = cum_disc(g,x,x[0])
+
+   if( max(F_err) > tol ):
+     print('Significant error in CDF')
 
    p = np.linspace(0.0,1.0,N)
-   #F_inv = invert_cdf_disc_cts(F, x, interpolate=True)
-   #G_inv = invert_cdf_disc_cts(G, x, interpolate=True)
    F_inv_disc = invert_cdf_disc_disc(F, x, p, 
      interpolate=False)
    G_inv_disc = invert_cdf_disc_disc(G, x, p, 
@@ -165,6 +115,7 @@ def wasserstein_integrand(f,g,x,N,smooth=False):
    diff = abs(F_inv_disc - G_inv_disc) ** 2
 
    return linearize(p, diff)
+
 #computes wasserstein distance
 # precondition -- f,g are probability densities
 # outputs W_2(f,g) where f,g are continuous and defined on the discrete
@@ -175,8 +126,6 @@ def wasserstein_distance(f,g,x,N):
  
 #compute direct wasserstein
 def partial_wasserstein(f,g,a,b,N):
-  #print('%s ----> %s'%(n,np.linalg.norm(f-g)))
-
   f_p, f_m = split_normalize(f,a,b)
   g_p, g_m = split_normalize(g,a,b)
 
@@ -217,46 +166,14 @@ def poly_wasserstein(f,g,a,b,N,norm_func):
   print('Renormalization array length = %s with W_2 = %s'%(len(rn_f_g), \
     np.sqrt(tot)))
   return np.sqrt(tot)
-  
-
-
-
-"""
-def test_convexity(f,shifts,a,b):
-  wass  = lambda g : partial_wasserstein(f,g,a,b)
-  dists = np.array(list(map(wass, shifts)))
-  return np.sqrt( np.sum(dists) )  
-"""
-
-#linearize a set of data points and their corresponding domain location
-def linearize(xx,yy):
-  def f(x):
-    left_end = -1
-    for i in range(0,len(xx)):
-      if( i == (len(xx) - 1) ):
-        print("WARNING: Possible domain error")
-      elif( xx[i] <= x and x <= xx[i+1] ):
-        left_end = i
-        break
-    if( left_end == -1 ):
-      return 0.0
-    left_val = yy[left_end]
-    dy       = yy[left_end+1] - yy[left_end]
-    dx       = xx[left_end+1] - xx[left_end]
-    delta_x  = x - xx[left_end]
-    fin      = left_val + dy/dx * delta_x
-    return fin
-  return f
 
 def shift(f,s):
   return lambda t : f(t-s)
 
-def trapz(ff, xx):
-  dx    = xx[1:] - xx[:-1]
-  ave_y = (ff[1:] + ff[:-1]) / 2
-  return np.dot(dx,ave_y)
 
-x = np.linspace(0,1,1000)
-f = lambda x : np.exp(x)
-ff = np.array(list(map(f,x)))
-print( trapz(ff, x) ) 
+def linearize(xx,ff):
+  def lin(x):
+     i = np.digitize(x,xx,False)
+     return ff[i-1] + (ff[i] - ff[i-1])\
+            / (xx[i]-xx[i-1]) * (x - xx[i-1])
+  return lin
--- src/1D/test_cum.py
+++ src/1D/test_cum.py
@@ -58,30 +58,32 @@ def gauss(s,sigma, c):
 def apply(f, x):
   return np.array(list(map(f,x)))
 
-def test_convexity(f,shifts,a,b,N):
-  x         = np.linspace(a,b,N)
+def test_convexity(f,shifts,norm_func,a,b,N):
+  #don't think I need x here since I'm not plotting
+#  x         = np.linspace(a,b,N)
+
+  #initialize data
   dists     = []
   L2_dists  = []
-  norm_func = rn.better_split_normalize
-  #gamma = -1.0
-  #norm_func = rn.exp_normalize(gamma)
-#  norm_func = rn.normalize_L1
   wass = lambda g : rn.poly_wasserstein(f,g,a,b,N,norm_func)
   L2   = lambda g : rn.L2_norm(f,g,a,b)
   for g in shifts:
     tmp  = wass(g)
-#    tmp  = [0.0, 0.0]
     tmp2 = L2(g)
     print('W2 Norm!!! = %s'%(tmp[0]))
     dists.append(tmp[0]**2)
     L2_dists.append(tmp2[0]**2)
   return dists, L2_dists
 
-def plot_convexity_test(f,shifts,s,a,b,N,fig_name='',the_title=''):
+def plot_convexity_test(f,s,norm_func,a,b,N,the_title='',fig_name=''):
+  #generate shifted functions and get distances
+  shifts = np.array(list(map(lambda t : lambda x: f(x-t), s)))
+  W2_dists,L2_dists = test_convexity(f,shifts, norm_func,a, b, N)
+  
+  #create prob. density plot
   x     = np.linspace(a,b,N)
-  dists,L2_dists = test_convexity(f,shifts, a, b, N)
-  print(dists)
   ff    = np.array(list(map(f,x)))
+
   shift_plots    = []
   for i,y in enumerate(shifts):
     if(i == 0 or i == np.floor(len(shifts)/2) or i == len(shifts) - 1):
@@ -91,15 +93,16 @@ def plot_convexity_test(f,shifts,s,a,b,N,fig_name='',the_title=''):
   fig.suptitle('Convexity Test: %s'%(the_title))
   for i,y in enumerate(shift_plots):
     axs[0].plot(x,y)
-  axs[1].plot(s,dists,label='W2(f,f_s)')
+  axs[1].plot(s,W2_dists,label='W2(f,f_s)')
   axs[2].plot(s,L2_dists,label='L_2^2(f,f_s)')
   axs[1].legend()
   axs[2].legend()
   fig.savefig(fig_name)
+  
+  return s,W2_dists,L2_dists
 
-
-mu        = [1.0, 3.0]
-sigma     = [0.2, 0.2]
+mu        = [0.0]
+sigma     = [0.2]
 K         = np.sqrt(3 * sigma[0]) * (math.pi ** (.25)) / 2
 c         = [K, K]
 
@@ -107,123 +110,27 @@ mu_step  = 0.04
 N_s      = 60
 
 N_x      = 1000
-h        = gauss(mu,sigma,c)
+da_gauss = gauss(mu,sigma,c)
 rick     = ricker(mu,sigma,c)
-a        = -4.0
-b        = 8.0
+a        = -5.0
+b        = 5.0
 shifted  = []
 rick_s   = []
 s        = mu_step * np.array(range(-N_s,N_s+1))
+norm_func = rn.better_split_normalize
 
-print(s)
-input()
-
-compute_gauss  = True
-compute_ricker = False
+#runtime decisions as to what we actually do
+compute_gauss  = False
+compute_ricker = True
 go = True
 
 if( compute_gauss and go ):
-  for shift_length in s:
-    print(shift_length)
-    shifted.append( gauss( mu + shift_length, sigma, c) )
- 
-  plt.figure(7)
-  xx = np.linspace(a,b,N_x)
-  FF,tmp = rn.cum_disc(h,xx)
-  hh = gauss(mu+mu_step * N_s*np.ones(2), sigma, c)
-  FFF,tmp = rn.cum_disc(hh,xx)
-  plt.title('Gaussian CDF')
-  plt.plot(xx,FF,xx,FFF)
-  plt.savefig('figures/Gauss/GaussCDF.png')
+  plot_convexity_test(da_gauss,s,norm_func,a,b,N_x,\
+                      'Ricker Convexity Test',\
+                      'figures/Gauss/GaussConvexityTest.png')
 
-  plt.figure(8)
-  yy = np.linspace(0.0,1.0,N_x)
-  F_inv = rn.invert_cdf_disc_disc(FF, xx, yy, True)
-  FFF_inv = rn.invert_cdf_disc_disc(FFF,xx,yy,True)
-  plt.title('Gaussian Quantile')
-  plt.plot(yy,F_inv,yy,FFF_inv)
-  plt.savefig('figures/Gauss/GaussInvCDF.png')
-   
-  """
-  plt.figure(4)
-  plot_convexity_test(h,                                  \
-                      shifted,                            \
-                      s,                                  \
-                      a, b, N_x,                          \
-                      fig_name ='figures/Gauss/GaussConvexity.png', \
-                      the_title= 'Gaussian Packets')
-  """
-
-if(compute_ricker and go):
-  plt.figure(5)
-  for s_len in s:
-    rick_s.append( ricker(mu + s_len, sigma, c) )
-  plot_convexity_test(rick,                                     \
-                      rick_s,                                     
-                      s,                                         \
-                      a, b, N_x,                                 \
-                      fig_name  = 'figures/RickerConvexity-Combo.png', \
-                      the_title = 'Ricker Wavelet')
-"""
-mu     = 0.0
-sigma  = 1.0
-shift  = 2.0
-c      = 1.0
-f      = ricker(mu,sigma,c)
-g      = ricker(mu + shift, sigma,c)
-N_x    = 1000
-N_y    = 1000
-a      = -5.0
-b      = 5.0
-x      = np.linspace(a,b,N_x)
-y      = np.linspace(0.0,1.0,N_y)
-
-rn_fg  = rn.better_split_normalize(f,g,a,b)
-tmp    = rn_fg[0]
-f      = tmp[0]
-g      = tmp[1]
-
-def get_stuff(h):
-  tmp    = rn.cum_disc(h,x)
-  F_disc = tmp[0]
-  F_inv  = rn.invert_cdf_disc_disc(F_disc, x, y, True)
-  F_inv2 = rn.invert_cdf_disc_disc(F_disc, x, y, False)
-  
-  F_inv_smooth = signal.savgol_filter(F_inv, 53, 5)
-  F_inv2_smooth = signal.savgol_filter(F_inv2, 53, 5)
-  return F_disc,F_inv,F_inv2,F_inv_smooth,F_inv2_smooth
-
-F_disc,F_inv,F_inv2,F_inv_smooth,F_inv2_smooth = get_stuff(f)
-G_disc,G_inv,G_inv2,G_inv_smooth,G_inv2_smooth = get_stuff(g)
-
-plt.figure(1)
-plt.plot(x,F_disc,label='F')
-plt.plot(x,G_disc,label='G')
-plt.title('CDF for Ricker+, support [-5,5], mu = 0.0, sigma = 1.0')
-plt.savefig('figures/cum_distribution.png')
-plt.legend()
-
-plt.figure(2)
-plt.plot(y,F_inv, label='I,NS')
-#plt.plot(y,F_inv2, label='NI,NS')
-plt.plot(y,G_inv, label='shift I,NS')
-#plt.plot(y,G_inv2, label='shift NI,NS')
-plt.title('Quantile Function for Ricker+, support [-5,5], mu = 0.0, sigma = 1.0')
-print('about to save')
-plt.savefig('figures/inv_nonsmooth.png')
-plt.legend()
-
-plt.figure(3)
-plt.plot(y,F_inv2, label='NI,NS')
-plt.plot(y,G_inv2, label='shift NI,NS')
-plt.savefig('figures/inv_interpolate_nonsmooth.png')
-plt.legend()
-
-plt.figure(4)
-plt.plot(y,F_inv_smooth, label='I,S')
-plt.plot(y,F_inv2_smooth, label='NI,S')
-plt.plot(y,G_inv_smooth, label='shift I,S')
-plt.plot(y, G_inv2_smooth, label='shift NI,S')
-plt.savefig('figures/inv_smooth.png')
-plt.legend()
-"""
+if( compute_ricker and go ):
+  s,W2_dists,L2_dists = plot_convexity_test(rick,s,norm_func,a,b,N_x,\
+                          'Ricker Convexity Test',\
+                          'figures/Ricker/RickerConvexityTest.png')
+  print(list(zip(s,W2_dists)))
